59bc80c762e7053f718d1b534d79b4b2d1812ec7
ChDmitryLv <d.charugin@gmail.com>
2021-03-19 00:25:43 +0200

    [DC]update user inputs

--- src/main/java/lv/training/inventory/ui/ProductUI.java
+++ src/main/java/lv/training/inventory/ui/ProductUI.java
@@ -12,6 +12,9 @@ import java.util.Scanner;
 
 
 public class ProductUI {
+
+    static Scanner sc = new Scanner(System.in);
+
     public void start(Database db, ProductService service) {
         boolean status = true;
         while (status) {
@@ -23,17 +26,13 @@ public class ProductUI {
                 case 4 -> updateProduct(service, db);
                 case 5 -> deleteProduct(service, db);
                 case 0 -> status = false;
-                default -> {}
+                default -> {
+                }
             }
         }
     }
 
-    private int chooseOperation() {
-        Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
-    }
-
-    public void menu() {
+    private void menu() {
         System.out.println("Choose operation:");
         System.out.println("1. save product");
         System.out.println("2. Find product by Id");
@@ -44,24 +43,61 @@ public class ProductUI {
         System.out.println("0. Exit");
     }
 
-    public void printResult(Product product){
+    private Integer chooseOperation() {
+        Integer operationNumber = null;
+        while (!sc.hasNextInt()) {
+            System.out.println("Only number allowed");
+            sc.next();
+        }
+        operationNumber = sc.nextInt();
+        return operationNumber;
+    }
+
+    private String titleInput(){
+        String title;
+        System.out.println("Set title: ");
+        title = sc.next();
+        return title;
+    }
+
+    private BigDecimal priceInput() {
+        BigDecimal price = null;
+        System.out.println("Product price:");
+        while (!sc.hasNextBigDecimal()) {
+            System.out.println("Price must be BigDecimal");
+            sc.next();
+        }
+        price = sc.nextBigDecimal();
+        return price;
+    }
+
+    private int categoryInput(){
+        int categoryNumber;
+        do {
+            System.out.println("Choose category: 1. FRUIT 2. DRINK 3. MEAL");
+            while (!sc.hasNextInt()){
+                System.out.println("Only number allowed");
+                sc.next();
+            }
+            categoryNumber = sc.nextInt();
+        } while (categoryNumber<1 || categoryNumber>3);
+            return categoryNumber;
+    }
+
+    public void printResult(Product product) {
         System.out.println(product);
     }
 
-    public void printResult(List<Product> list){
-        for (Product it: list){
+    public void printResult(List<Product> list) {
+        for (Product it : list) {
             printResult(it);
         }
     }
 
     ProductInput productInput() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Product title: ");
-        String name = sc.nextLine();
-        System.out.println("Product price: ");
-        BigDecimal price = sc.nextBigDecimal();
-        System.out.println("Choose category: 1. FRUIT 2. DRINK 3. MEAL");
-        int categoryNumber = sc.nextInt();
+        String name = titleInput();
+        BigDecimal price = priceInput();
+        int categoryNumber = categoryInput();
         Category category = switch (categoryNumber) {
             case 1 -> Category.FRUIT;
             case 2 -> Category.DRINK;
@@ -72,8 +108,8 @@ public class ProductUI {
         return new ProductInput(name, price, category);
     }
 
-    void createProduct(ProductService service, Database db){
-        service.create(productInput(),db);
+    void createProduct(ProductService service, Database db) {
+        service.create(productInput(), db);
     }
 
     void findProduct(ProductService service, Database db) {
@@ -84,22 +120,22 @@ public class ProductUI {
         printResult(product);
     }
 
-    void printAll(ProductService service, Database db){
+    void printAll(ProductService service, Database db) {
         List<Product> list = service.printAll(db);
         printResult(list);
     }
 
-    void updateProduct(ProductService service, Database db){
+    void updateProduct(ProductService service, Database db) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Set product Id to change");
         int id = sc.nextInt();
         service.find(id, db);
         ProductInput updatedProduct = productInput();
-        service.update(db,updatedProduct,id);
+        service.update(db, updatedProduct, id);
     }
 
-    void deleteProduct(ProductService service, Database db){
-        printAll(service,db);
+    void deleteProduct(ProductService service, Database db) {
+        printAll(service, db);
         System.out.println("Choose product Id to delete: ");
         Scanner sc = new Scanner(System.in);
         int id = sc.nextInt();
