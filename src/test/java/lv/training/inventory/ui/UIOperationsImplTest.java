fd3c6afc23e045c1bf3f6982b2a55aa183cf9be6
ChDmitryLv <d.charugin@gmail.com>
2021-04-03 14:06:22 +0300

    [DC] clean

--- src/test/java/lv/training/inventory/ui/UIOperationsImplTest.java
+++ src/test/java/lv/training/inventory/ui/UIOperationsImplTest.java
@@ -6,10 +6,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
